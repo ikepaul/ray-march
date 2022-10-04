134df4dea2b89c79b10ec031afc9d44d401ab987
IKE <paul.e.soukup@gmail.com>
2022-10-04 13:34:38 +0200

    used Point instead

--- shapes.hs
+++ shapes.hs
@@ -1,6 +1,6 @@
 module Shapes where
 
-import Graphics.UI.Threepenny (Color (RGB))
+import Graphics.UI.Threepenny (Color (RGB), Point)
 
 data Shape
   = Rectangle {shapeX :: Double, shapeY :: Double, shapeWidth :: Double, shapeHeight :: Double, shapeColor :: Color}
@@ -34,7 +34,7 @@ myShapes :: [Shape]
 myShapes = [myRectangle, myCircle, myLine, myLine2]
 
 distance :: (Double, Double) -> Shape -> Double
-distance (x1, y1) (Circle x2 y2 r _) = abs( (- r) + sqrt ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)))
+distance (x1, y1) (Circle x2 y2 r _) = abs ((- r) + sqrt ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)))
 distance (x1, y1) (Rectangle x2 y2 w h _) = minimum sides
   where
     sides =
@@ -72,3 +72,17 @@ distance (x1, y1) (Line x2 y2 x3 y3 w _) = minimum sides
         distance (x1, y1) (Line (x3 + (w / 2) * cos angle) (y3 + (w / 2) * sin angle) (x3 - (w / 2) * cos angle) (y3 - (w / 2) * sin angle) 0 undefined)
       ]
     angle = if (x3 - x2) /= 0 then atan ((y3 - y2) / (x3 - x2)) + pi * 0.5 else pi
+
+getIntersection :: Point -> Point -> Point -> Point -> Point
+getIntersection (x1, y1) (x2, y2) (x3, y3) (x4, y4) = (x5, y5)
+  where
+    x5 = lerp x1 x2 t
+    y5 = lerp y1 y2 t
+    t = tTop / bottom
+    u = uTop / bottom
+    tTop = (x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)
+    uTop = (y3 - y1) * (x1 - x2) - (x3 - x1) * (y1 - y2)
+    bottom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1)
+
+lerp :: Double -> Double -> Double -> Double
+lerp a b t = a + (b - a) * t
